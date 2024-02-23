91428c14b04ce3ac4bca0bd4a484be2aeff81c14
Mark Susol <marksusol@gmail.com>
2024-02-23 12:07:34 -0700

    initial brute force approach, baseline process to iterate from

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# vorto
+# Vorto Algorithmic Challenge
+
+For this challenge, you will submit a program that solves a version of the **Vehicle Routing
+Problem (VRP)**.
+
+The VRP specifies a set of loads to be completed efficiently by an unbounded number of
+drivers.
+
+Each load has a pickup location and a dropoff location, each specified by a Cartesian point. A
+driver completes a load by driving to the pickup location, picking up the load, driving to the
+dropoff, and dropping off the load. The time required to drive from one point to another, in
+minutes, is the Euclidean distance between them. That is, to drive from (x1, y1) to (x2,
+y2) takes `sqrt((x2-x1)^2 + (y2-y1)^2)` minutes.
+
+As an example, suppose a driver located at (0,0) starts a load that picks up at (50,50) and
+delivers at (100,100). This would take `2*sqrt(2*50^2) = ~141.42 minutes` of drive time
+to complete: `sqrt((50-0)^2 + (50-0)^2)` minutes to drive to the pickup, and
+`sqrt((100-50)^2 + (100-50)^2)` minutes to the dropoff.
+
+Each driver starts and ends his shift at a depot located at (0,0). A driver may complete
+multiple loads on his shift, but may not exceed 12 hours of total drive time. That is, the total
+Euclidean distance of completing all his loads, including the return to (0,0), must be less than
+12*60.
+
+A VRP solution contains a list of drivers, each of which has an ordered list of loads to be
+completed. All loads must be assigned to a driver.
+
+The total cost of a solution is given by the formula:
+
+```python
+total_cost = 500*number_of_drivers + total_number_of_driven_minutes
+```
+
+A good program will produce a solution with a low total cost, but does not take too long to run
+(see Evaluation section below).
+
+**CVRPPD (Capacitated Vehicle Routing Problem with Pickup and Delivery)**
+
+Interesting Read: [A Hybrid Genetic Algorithm for Solving the VRP with Pickup and Delivery
+in Rural Areas](https://scholarspace.manoa.hawaii.edu/server/api/core/bitstreams/6e73b15f-13ae-489a-b8b9-3a4fcb9e57ba/content)
+
+## Evaluate Training Problems
+
+```bash
+$ python3 evaluateShared.py --cmd "python3 vrp.py --inputPath" --problemDir "TrainingProblems"
+```
+
+| Approach               | mean cost           | mean run time         |
+|------------------------|---------------------|-----------------------|
+| get_brute_force_routes | 90138.6429881012    | 103.60167026519775ms  |
+
+### Demo
+
+Taken from the challenge documentation, used for iterating on code directly.
+
+given: `TestingProblems/demo.txt`
+
+```
+loadNumber pickup dropoff
+1 (-50.1,80.0) (90.1,12.2)
+2 (-24.5,-19.2) (98.5,1.8)
+3 (0.3,8.9) (40.9,55.0)
+4 (5.3,-61.1) (77.8,-5.4)
+```
+
+```shell
+$ python3 vrp.py --inputPath "TestingProblems/demo.txt"
+
+[1, 2]
+[3, 4]
+```
---
+++ vrp.py
@@ -0,0 +1,108 @@
+import argparse
+import math
+import numpy as np
+from collections import defaultdict
+from typing import List
+
+import evaluateShared as ev
+
+class Solver():
+    TIME_CONSTRAINT = 12*60
+    DEPOT = ev.Point(0,0)
+
+    def __init__(self, inputPath):
+        self.inputPath = inputPath
+        self.routes = defaultdict(list)
+        self.problem = ev.loadProblemFromFile(inputPath)
+        # TODO: eliminate self.problem.loads as args.
+        self.loadMatrix = self.calculate_load_matrix(self.problem.loads) # M
+        self.depotDistOut = self.calculate_depot_distances(self.problem.loads, depot_out=True) # Dout
+        self.depotDistBack = self.calculate_depot_distances(self.problem.loads, depot_out=False) # Dback
+
+    def calculate_load_matrix(self, loads):
+        """Calculate a 'pseudo' distance matrix for loads."""
+        # TODO: Can we do this greedily when reading file into problem?
+        n = len(loads)
+        load_matrix = np.zeros((n, n))
+
+        for i in range(n):
+            for j in range(n):
+                load_matrix[i, j] = ev.distanceBetweenPoints(self.problem.loads[i].dropoff, self.problem.loads[j].pickup)
+        return load_matrix
+
+    def calculate_depot_distances(self, loads, depot_out=True):
+        """Calculate the distance matrix for loads to/from depot."""
+        n = len(loads)
+        dist_array = np.zeros(n)
+
+        for i in range(n):
+            if depot_out:
+                dist_array[i] = ev.distanceBetweenPoints(self.DEPOT, self.problem.loads[i].pickup)
+            else:
+                dist_array[i] = ev.distanceBetweenPoints(self.problem.loads[i].dropoff, self.DEPOT)
+        return dist_array
+
+    def get_route_distance(self, schedule: List[ev.Load]) -> int:
+        """Get full route distance of schedule.
+        Ex: [1,3] load.id - 1 = matrix (M) offset
+          distance = Dout[0] + M[0][0] + M[0][2] + M[2][2] + Dback[2]
+        """
+        assert len(schedule) > 0
+
+        def matrix_offset(i):
+            # Convert load.id: str -> int
+            return int(schedule[i].id)-1
+
+        # Depot out (Dout)
+        distance = self.depotDistOut[matrix_offset(0)]
+        for idx in range(len(schedule)):
+            # Load Demand
+            #print('M[%s][%s]'%(matrix_offset(idx), matrix_offset(idx)))
+            distance += self.loadMatrix[matrix_offset(idx)][matrix_offset(idx)]
+            # Previous load deadhead
+            if idx != 0:
+                #print('M[%s][%s]'%(matrix_offset(idx-1), matrix_offset(idx)))
+                distance += self.loadMatrix[matrix_offset(idx-1)][matrix_offset(idx)]
+        # Depot back (Dback)
+        distance += self.depotDistBack[matrix_offset(idx)]
+
+        return distance
+
+    # Work In Progress
+    def get_closest_load_id(self, M, load):
+        """ Closet load in load matrix, M, by load.id."""
+        offset = load - 1
+        min_distance = math.inf
+        min_idx = None
+        for idx, val in np.ndenumerate(M[offset]):
+            if idx[0] != offset:
+                if val < min_distance:
+                    min_distance = val
+                    min_idx = idx[0] + 1
+        return min_idx
+
+    def get_brute_force_routes(self):
+        driver = 1
+        schedule = []
+        for load in self.problem.loads:
+            schedule.append(load)
+            distance = self.get_route_distance(schedule)
+            if distance > self.TIME_CONSTRAINT:
+                schedule = [load]
+                driver += 1
+            self.routes[driver].append(load)
+
+    def print_routes(self):
+        for route in self.routes.items():
+            print([int(load.id) for load in route[1]])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputPath", help="Path to file containing problem", required=True)
+    args=parser.parse_args()
+
+    vrp = Solver(args.inputPath)
+    vrp.get_brute_force_routes()
+    # Print to stdout
+    vrp.print_routes()
